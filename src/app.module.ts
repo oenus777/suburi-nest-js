e41c9524773089ee8b7e612b6d3a91e5dd3d9821
つた <30628476+oenus777@users.noreply.github.com>
2025-01-18 13:14:55 +0000

    add: TypeORMを使用してPostgreSQLデータベースの接続設定を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true, // 開発環境でのみtrueにすることをお勧めします
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
